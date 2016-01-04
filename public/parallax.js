abb5c167f318286a397f0ad5e281e7c3929fe10e
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-01-04 16:03:00 +0100

    Adds some keyframes

--- public/parallax.js
+++ public/parallax.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
-  
+
+  var keyframes = [
+    {
+      "wrapper" : "#introduction .animation",
+      "duration" : "100%",
+      "animations" :  [
+        {
+          "selector"    : "#title",
+          "translateY"  : -140,
+          "opacity"     : 0
+        },
+        {
+          "selector"    : "#subtitle",
+          "translateY"  : -110,
+          "opacity"     : 0
+        }
+      ]
+    }
+  ]
+
 })
